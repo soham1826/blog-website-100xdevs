40ca0f1c2f727f2c21abc42d144eb12d738944c2
soham Kulkarni <soham18262@gmail.com>
2024-04-17 14:08:59 +0530

    error fixing

--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -1,29 +1,62 @@
 
-import AppBar from "../components/AppBar"
-import BlogCard from "../components/BlogCard"
-import { useBlogs } from "../hooks"
+// import AppBar from "../components/AppBar"
+// import BlogCard from "../components/BlogCard"
+// import { useBlogs } from "../hooks"
+
+// const Blogs = () => {
+//     const{loading, blogs} = useBlogs();
+//     console.log(blogs);
+
+//     if(loading){
+//         return <div>
+//             loading...
+//         </div>
+//     }
+
+//     return (
+//     <div className="overflow-x-hidden">
+//     <AppBar/>
+//     <div className="w-screen flex items-center justify-center">
+//         <div className="flex flex-col w-full md:max-w-6xl" >
+//         {blogs.map((blog,index)=><BlogCard key={index} id={blog.id} username={blog.author.username} title={blog.title} publishedDate="Mar 12,2024"  content={blog.content}/>)}
+        
+//         </div>
+//     </div>
+//     </div>
+//   )
+// }
+
+import AppBar from "../components/AppBar";
+import BlogCard from "../components/BlogCard";
+import { useBlogs } from "../hooks";
 
 const Blogs = () => {
-    const{loading, blogs} = useBlogs();
+    const { loading, blogs } = useBlogs();
     console.log(blogs);
 
-    if(loading){
-        return <div>
-            loading...
-        </div>
+    if (loading) {
+        return <div>loading...</div>;
     }
 
     return (
-    <div className="overflow-x-hidden">
-    <AppBar/>
-    <div className="w-screen flex items-center justify-center">
-        <div className="flex flex-col w-full md:max-w-6xl" >
-        {blogs.map((blog,index)=><BlogCard key={index} id={blog.id} username={blog.author.username} title={blog.title} publishedDate="Mar 12,2024"  content={blog.content}/>)}
-        
+        <div className="overflow-x-hidden">
+            <AppBar />
+            <div className="w-screen flex items-center justify-center">
+                <div className="flex flex-col w-full md:max-w-6xl">
+                    {blogs.map((blog, index) => (
+                        <BlogCard
+                            key={blog.id ?? index} // Ensure blog.id is not undefined
+                            id={blog.id ?? 0} // Assuming id is a number, replace 0 with a suitable default value if needed
+                            username={blog.author?.username ?? "Unknown"} // Use optional chaining to avoid null/undefined error
+                            title={blog.title ?? "Untitled"} // Use default value if title is undefined
+                            publishedDate="Mar 12, 2024" // Assuming this is a static value for now
+                            content={blog.content ?? ""} // Use default value if content is undefined
+                        />
+                    ))}
+                </div>
+            </div>
         </div>
-    </div>
-    </div>
-  )
-}
+    );
+};
 
-export default Blogs
+export default Blogs;
